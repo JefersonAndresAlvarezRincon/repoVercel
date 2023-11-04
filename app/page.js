02acd3667c7966aca9420360b5cf9d779a36ee10
“Jeferson <jeferson.alvarez101000@gmail.com>
2023-11-04 04:23:32 -0500

    primerooo

--- app/page.js
+++ app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Docs XDDDDDDDDDD <span>-&gt;</span>
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
@@ -59,7 +59,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Learn  XDDDDDDDDDDD<span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
